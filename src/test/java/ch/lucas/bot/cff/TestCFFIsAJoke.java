020f4a5d9f96866ff3348e47dc03f85ba3e1adc3
Lucas <32235434+lucas-it@users.noreply.github.com>
2020-09-29 16:26:15 +0200

    fix: rename file to TestCFFIsAJoke from CFFIsAJoke

--- src/test/java/ch/lucas/bot/cff/TestCFFIsAJoke.java
+++ src/test/java/ch/lucas/bot/cff/TestCFFIsAJoke.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-public class CFFIsAJoke {
+public class TestCFFIsAJoke {
     @Test
     public void testGetInformationFromApiMethodDontMakeException() {
         try {
