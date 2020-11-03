29ccae024baceb01f5f4d250373e5cfd313dc9da
Lucas <32235434+lucas-it@users.noreply.github.com>
2020-11-03 11:48:54 +0100

    feat: add tests for better coverage

--- src/test/java/ch/lucas/bot/cff/utils/cffapi/TestDisruptionStats.java
+++ src/test/java/ch/lucas/bot/cff/utils/cffapi/TestDisruptionStats.java
@@ -10,11 +10,24 @@ public class TestDisruptionStats {
 
     @Test
     public void testDisruptionStatsNbrOfDeletedTravels() {
+        disruptionStats.setNumberOfDelayedTravels(nbrOfDelayedTravels);
         Assertions.assertEquals(nbrOfDelayedTravels, disruptionStats.getNumberOfDelayedTravels());
     }
 
     @Test
     public void testDisruptionStatsCumulativeLate() {
+        disruptionStats.setCumulativeLate(cumulativeLate);
         Assertions.assertEquals(cumulativeLate, disruptionStats.getCumulativeLate());
+    }
+
+    @Test
+    public void testDisruptionsStatsNbrOfDeletedTravelsSetMethod() {
+        disruptionStats.setNumberOfDelayedTravels(123456);
+        Assertions.assertEquals(123456, disruptionStats.getNumberOfDelayedTravels());
+    }
+    @Test
+    public void testDisruptionsStatsCumulativeLateSetMethod() {
+        disruptionStats.setCumulativeLate(789123);
+        Assertions.assertEquals(789123, disruptionStats.getCumulativeLate());
     }
 }
