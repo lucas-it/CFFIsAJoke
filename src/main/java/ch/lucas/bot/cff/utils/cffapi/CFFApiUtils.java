b69bf1f98b1098144356a2cf4ccbc652c8a7eb50
Lucas <32235434+lucas-it@users.noreply.github.com>
2021-06-06 15:44:54 +0200

    feat: test CFFApiUtils class

--- src/main/java/ch/lucas/bot/cff/utils/cffapi/CFFApiUtils.java
+++ src/main/java/ch/lucas/bot/cff/utils/cffapi/CFFApiUtils.java
@@ -46,7 +46,7 @@ public class CFFApiUtils {
     /**
      * Get the information about disruptions from the SBB api about the precedent day.
      * @return a Message object
-     * @throws IOException error while connecting to the SBB API
+     * @throws InterruptedException error while connecting to the SBB API
      */
     public Message getInformationFromAPI() throws InterruptedException {
         Thread t1 = new Thread(() -> {
@@ -105,7 +105,7 @@ public class CFFApiUtils {
      * @throws IOException error while connecting to the SBB API
      */
     private DisruptionStats getDisruptionStats() throws IOException {
-        // Obtaining travels that are late
+        // Obtaining late travels
         LOGGER.info("getDisruptionStats - Initialize connection to SBB API");
         URL url = new URL("https://data.sbb.ch/api/records/1.0/search/?dataset=ist-daten-sbb&q=&rows=10000&sort=-linien_id&facet=betreiber_id&facet=produkt_id&facet=linien_id&facet=linien_text&facet=verkehrsmittel_text&facet=faellt_aus_tf&facet=bpuic&facet=ankunftszeit&facet=an_prognose&facet=an_prognose_status&facet=ab_prognose_status&facet=ankunftsverspatung&facet=abfahrtsverspatung&refine.produkt_id=Zug&refine.ankunftsverspatung=true&apikey=" + config.getSBBApiKey());
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -115,22 +115,30 @@ public class CFFApiUtils {
         LOGGER.info("getDisruptionStats - Read API response");
         BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
         LOGGER.info("getDisruptionStats - Parse API response to JSON");
-        JsonArray recordsLates = JsonParser.parseReader(br).getAsJsonObject().getAsJsonArray("records");
+        JsonArray recordsLate = JsonParser.parseReader(br).getAsJsonObject().getAsJsonArray("records");
 
+        return parseDisruptionStatsFromJson(recordsLate);
+    }
+
+    /**
+     * Parse disruptions stats (number of late travels and cumulative late) from json provided by the API.
+     * @param recordsLate json array of late trains
+     * @return disruption stats
+     */
+    public DisruptionStats parseDisruptionStatsFromJson(JsonArray recordsLate) {
         long cumulativeLate = 0;
         int numberOfDelayedTravels = 0;
         List<TrainLate> delayedTrains = new ArrayList<>();
 
-        LOGGER.info("getDisruptionStats - Process late travels data");
-        for(JsonElement jsonElement : recordsLates) {
+        LOGGER.info("parseDisruptionStatsFromJson - Process late travels data");
+        for(JsonElement jsonElement : recordsLate) {
             try {
                 String recordId = jsonElement.getAsJsonObject().get("recordid").getAsString();
                 JsonObject fields = jsonElement.getAsJsonObject().get("fields").getAsJsonObject();
                 int lineId = fields.get("linien_id").getAsInt();
                 String stopName = fields.get("haltestellen_name").getAsString();
                 Date arrivedProgrammedDate = simpleDateFormat.parse(fields.get("ankunftszeit").getAsString());
-                Date arrivedDate = null;
-                arrivedDate = simpleDateFormat.parse(fields.get("an_prognose").getAsString());
+                Date arrivedDate = simpleDateFormat.parse(fields.get("an_prognose").getAsString());
 
                 delayedTrains.add(new TrainLate(recordId, stopName, lineId, arrivedDate, arrivedProgrammedDate));
             } catch(ParseException e) {
@@ -149,6 +157,7 @@ public class CFFApiUtils {
 
         numberOfDelayedTravels = delayedTrainsPerLineId.size();
 
+        LOGGER.info("parseDisruptionStatsFromJson - Late travels data processed");
         return new DisruptionStats(numberOfDelayedTravels, cumulativeLate);
     }
 
@@ -158,8 +167,8 @@ public class CFFApiUtils {
      * @throws IOException error while connecting to the SBB API
      */
     private int getDeletedTravels() throws IOException {
-        // Obtaining travels that are late
-        LOGGER.info("getDisruptionStats - Initialize connection to SBB API");
+        // Obtaining deleted travels
+        LOGGER.info("getDeletedTravels - Initialize connection to SBB API");
         URL url = new URL("https://data.sbb.ch/api/records/1.0/search/?dataset=ist-daten-sbb&q=&rows=10000&sort=-linien_id&facet=betreiber_id&facet=linien_id&facet=faellt_aus_tf&facet=ankunftszeit&facet=an_prognose&facet=ankunftsverspatung&facet=abfahrtsverspatung&refine.faellt_aus_tf=true&apikey=" + config.getSBBApiKey());
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
@@ -168,14 +177,22 @@ public class CFFApiUtils {
         LOGGER.info("getDeletedTravels - Read API response");
         BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
         LOGGER.info("getDeletedTravels - Parse API response to JSON");
-        JsonArray recordsLates = JsonParser.parseReader(br).getAsJsonObject().getAsJsonArray("records");
+        JsonArray recordsDeleted = JsonParser.parseReader(br).getAsJsonObject().getAsJsonArray("records");
 
-        // Defin number of lates
+        return parseDeletedTravelsFromJson(recordsDeleted);
+    }
+
+    /**
+     * Parse number of deleted travels from json provided by the API.
+     * @param recordsDeleted json array of deleted travels
+     * @return number of deleted travels
+     */
+    public int parseDeletedTravelsFromJson(JsonArray recordsDeleted) {
         int nbrOfDeletedTravels = 0;
         int lastLineId = 0;
 
-        LOGGER.info("getDeletedTravels - Process deleted travels data");
-        for(JsonElement jsonElement : recordsLates) {
+        LOGGER.info("parseDeletedTravelsFromJson - Process deleted travels data");
+        for(JsonElement jsonElement : recordsDeleted) {
             JsonObject fields = jsonElement.getAsJsonObject().get("fields").getAsJsonObject();
             int lineId = fields.get("linien_id").getAsInt();
 
@@ -186,6 +203,7 @@ public class CFFApiUtils {
             lastLineId = lineId;
         }
 
+        LOGGER.info("parseDeletedTravelsFromJson - Deleted travels data processed");
         return nbrOfDeletedTravels;
     }
 
@@ -206,18 +224,27 @@ public class CFFApiUtils {
         BufferedReader allDataJSONReader = new BufferedReader(new InputStreamReader(allDataJSONConn.getInputStream()));
         LOGGER.info("getTotalTravels - Parse API response to JSON");
         JsonReader jsonReader = new JsonReader(allDataJSONReader);
-        JsonArray records = JsonParser.parseReader(jsonReader).getAsJsonArray();
+        JsonArray travels = JsonParser.parseReader(jsonReader).getAsJsonArray();
 
+        return parseTotalTravelsFromJson(travels);
+    }
+
+    /**
+     * Parse number of total travels from json provided by the API.
+     * @param travels json array of all travels
+     * @return number of travels
+     */
+    public int parseTotalTravelsFromJson(JsonArray travels) {
         List<Integer> linesId = new ArrayList<>();
 
-        LOGGER.info("getTotalTravels - Process data");
-        StreamSupport.stream(records.spliterator(), true).parallel().map(r -> r.getAsJsonObject().get("fields").getAsJsonObject().get("linien_id").getAsInt()).forEach(l -> {
+        LOGGER.info("parseTotalTravelsFromJson - Process number of travels");
+        StreamSupport.stream(travels.spliterator(), true).parallel().map(r -> r.getAsJsonObject().get("fields").getAsJsonObject().get("linien_id").getAsInt()).forEach(l -> {
             if(!linesId.contains(l)) linesId.add(l);
         });
 
         int nbrOfTravels = linesId.size();
 
-        LOGGER.info("getTotalTravels - Return {} travels", nbrOfTravels);
+        LOGGER.info("parseTotalTravelsFromJson - Number of travels processed");
         return nbrOfTravels;
     }
 }
---
+++ src/test/java/ch/lucas/bot/cff/utils/cffapi/TestCFFApiUtils.java
@@ -0,0 +1,45 @@
+package ch.lucas.bot.cff.utils.cffapi;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCFFApiUtils {
+    private CFFApiUtils cffApiUtils = new CFFApiUtils(null);
+
+    @Test
+    public void testParseDisruptionStatsFromJson() {
+        InputStream disruptions = getClass().getResourceAsStream("/disruptions.json");
+        JsonArray disruptionsJson = JsonParser.parseReader(new InputStreamReader(disruptions)).getAsJsonArray();
+
+        DisruptionStats underTest = cffApiUtils.parseDisruptionStatsFromJson(disruptionsJson);
+
+        assertEquals(9, underTest.getNumberOfDelayedTravels());
+        assertEquals(7567000, underTest.getCumulativeLate());
+    }
+
+    @Test
+    public void testParseDeletedTravelsFromJson() {
+        InputStream deletedTravels = getClass().getResourceAsStream("/deletedTravels.json");
+        JsonArray deletedTravelsJson = JsonParser.parseReader(new InputStreamReader(deletedTravels)).getAsJsonArray();
+
+        int underTest = cffApiUtils.parseDeletedTravelsFromJson(deletedTravelsJson);
+
+        assertEquals(8, underTest);
+    }
+
+    @Test
+    public void testParseTotalTravelsFromJson() {
+        InputStream travels = getClass().getResourceAsStream("/travels.json");
+        JsonArray travelsJson = JsonParser.parseReader(new InputStreamReader(travels)).getAsJsonArray();
+
+        int underTest = cffApiUtils.parseTotalTravelsFromJson(travelsJson);
+
+        assertEquals(6, underTest);
+    }
+}
